1b5014f289c4372e209f91bdcbd3058b490b17bb
rafal <ra88fi@o2.pl>
2017-05-19 20:01:39 +0200

    repaired gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,41 +3,49 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
   	sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
-          'style.css': 'style.sass'
-        }
-      }
-    }
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'main.css': 'main.sass'
+  			}
+  		}
+  	},
 
-    imagemin: {
-    	dynamic: {
-        	files: [{
-            	expand: true,
-            	cwd: '',
-            	src: ['**/*.{png,jpg,gif}'],
-            	dest: ''
-        	}]
-    	}
-	}
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/build/'
+  			}]
+  		}
+  	},
 
-	watch: {
-    scripts: {
-        files: ['.html'],
-        tasks: ['.html'],
-        options: {
-            spawn: false,
-        },
-    } 
-}
+  	watch: {
+    	scripts: {
+        	files: ['sass/*.sass'],
+        	tasks: ['sass'],
+        	options: {
+            	spawn: false,
+        	},
+    	} 
+	},
+      
+    
+      
+      
+
+
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+
   // Default task(s).
-  grunt.registerTask('default', ["sass", 'imagemin', 'watch']);
+  
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
